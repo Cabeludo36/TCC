6c212e82777bb02c6f20ad7518de10ec9a2ef4f4
Cabeludo <brepimentel2001@gmail.com>
2023-11-01 20:13:33 -0300

    cria input monetario

---
+++ components/shared/CurrencyInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--@vue-ignore-->
+  <input
+    :class="$attrs.class"
+    :placeholder="$attrs.placeholder"
+    ref="inputRef"
+    type="text"
+  />
+</template>
+
+<script setup lang="ts">
+import type { PropType } from "vue";
+import {
+  useCurrencyInput,
+  type CurrencyInputOptions,
+} from "vue-currency-input";
+const props = defineProps({
+  modelValue: Number,
+  options: Object as PropType<CurrencyInputOptions>,
+});
+const defOptions: CurrencyInputOptions = {
+  locale: "pt-BR",
+  currency: "BRL",
+  precision: 2,
+  hideCurrencySymbolOnFocus: true,
+  hideGroupingSeparatorOnFocus: true,
+  hideNegligibleDecimalDigitsOnFocus: true,
+  autoDecimalDigits: false,
+  useGrouping: false,
+  accountingSign: false,
+};
+const { inputRef } = useCurrencyInput(
+  props.options ? props.options : defOptions
+);
+</script>
