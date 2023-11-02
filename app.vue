8984a7e93f5916c7a4bd445a09e067db06dfa4ea
Cabeludo <brepimentel2001@gmail.com>
2023-11-01 21:16:42 -0300

    padding para tela menores

--- app.vue
+++ app.vue
@@ -8,7 +8,7 @@ useHead({
 });
 </script>
 <template>
-  <div class="md:p-8">
+  <div class="p-2 md:p-8">
     <div class="flex flex-col w-full border-opacity-50">
       <div class="grid card bg-base-300 rounded-box place-items-center md:p-3">
         <div class="flex flex-col md:flex-row w-full">
