7cdf852e1cf8f70f5962a8c3ee1f463ec543acc1
Cabeludo <brepimentel2001@gmail.com>
2023-11-02 19:57:30 -0300

    apenas 2 digitos demimais

--- components/geral.vue
+++ components/geral.vue
@@ -13,15 +13,15 @@ const props = defineProps({
 <template>
 <div class="flex flex-col w-full justify-center">
     <div class="flex flex-col items-center mr-3 ml-3 text-lg">
-        <span class="font-semibold">R$ {{ dispesasRendaResumo.renda }}</span>
+        <span class="font-semibold">R$ {{ dispesasRendaResumo.renda.toFixed(2) }}</span>
         <h4 class="text-blue-500 font-semibold">TOTAL RENDA MENSAL</h4>
     </div>
     <div class="flex flex-col items-center mr-3 ml-3 text-base">
-        <span class="font-semibold">R$ {{ dispesasRendaResumo.dispeas }}</span>
+        <span class="font-semibold">R$ {{ dispesasRendaResumo.dispeas.toFixed(2) }}</span>
         <h4 class="text-blue-500 font-semibold">TOTAL DESPESA MENSAL</h4>
     </div>
     <div class="flex flex-col items-center mr-3 ml-3 text-lg">
-        <span class="font-semibold">R$ {{ dispesasRendaResumo.diferenca }}</span>
+        <span class="font-semibold">R$ {{ dispesasRendaResumo.diferenca.toFixed(2) }}</span>
         <h4 class="text-blue-500 font-semibold">TOTAL SALDO CAIXA</h4>
     </div>
 </div>
